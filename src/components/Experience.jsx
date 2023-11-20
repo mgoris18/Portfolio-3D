3fc428f4474d5e11683959b0af6afc9288d1d445
Michell <72279305+mgoris18@users.noreply.github.com>
2023-11-20 11:01:40 -0400

    assets and components

---
+++ src/components/Experience.jsx
@@ -0,0 +1,206 @@
+import {
+  Float,
+  MeshDistortMaterial,
+  MeshWobbleMaterial,
+  useScroll,
+} from "@react-three/drei";
+import { useFrame, useThree} from "@react-three/fiber";
+import { animate, useMotionValue } from "framer-motion";
+import { motion } from "framer-motion-3d";
+import { useEffect, useRef, useState } from "react";
+import { framerMotionConfig } from "../config";
+import { Avatar } from "./Avatar";
+import { Office } from "./Office";
+import { Projects } from "./Projects";
+import { Background } from "./Background";
+import * as THREE from "three";
+
+
+export const Experience = (props) => {
+  const { menuOpened } = props;
+  const { viewport } = useThree();
+  const data = useScroll();
+
+  const isMobile = window.innerWidth < 768;
+  const responsiveRatio = viewport.width / 12;
+  const officeScaleRatio = Math.max(0.5, Math.min(0.9 * responsiveRatio, 0.9));
+
+  const [section, setSection] = useState(0);
+
+  const cameraPositionX = useMotionValue();
+  const cameraLookAtX = useMotionValue();
+
+  useEffect(() => {
+    animate(cameraPositionX, menuOpened ? -5 : 0, {
+      ...framerMotionConfig,
+    });
+    animate(cameraLookAtX, menuOpened ? 5 : 0, {
+      ...framerMotionConfig,
+    });
+    
+  }, [menuOpened]);
+
+  const characterContainerAboutRef = useRef();
+
+  const [characterAnimation, setCharacterAnimation] = useState("Typing");
+  useEffect(() => {
+    setCharacterAnimation("Falling");
+    setTimeout(() => {
+      setCharacterAnimation(section === 0 ? "Typing" : "Standing");
+    }, 600);
+  }, [section]);
+
+  const characterGroup = useRef();
+
+  useFrame((state) => {
+    let curSection = Math.floor(data.scroll.current * data.pages);
+
+    if (curSection > 3) {
+      curSection = 3;
+    }
+
+    if (curSection !== section) {
+      setSection(curSection);
+    }
+
+
+    state.camera.position.x = cameraPositionX.get();
+    state.camera.lookAt(cameraLookAtX.get(), 0, 0);
+
+    //const position = new THREE.Vector3();
+    if (section === 0) {
+      characterContainerAboutRef.current.getWorldPosition(characterGroup.current.position);
+      }
+    //console.log([position.x, position.y, position.z])
+
+    //const quaternion = new THREE.Quaternion();
+    //characterContainerAboutRef.current.getWorldQuaternion(quaternion);
+    //const euler = new THREE.Euler();
+    //euler.setFromQuaternion(quaternion, "XYZ");
+
+    //console.log([euler.x, euler.y, euler.z]);
+  });
+
+
+  return (
+    <>
+      <Background />
+      <motion.group 
+        ref ={characterGroup}
+        rotation = {[-3.141592653589793, 0.8261944901923446, -3.141592653589793]}
+        scale={[officeScaleRatio, officeScaleRatio, officeScaleRatio]}
+        animate={"" + section}
+        transition= {{
+          duration: 0.6,
+        }}
+        variants = {{
+          o: {
+            scaleX: officeScaleRatio,
+            scaleY: officeScaleRatio,
+            scaleZ: officeScaleRatio,
+          },
+          1: {
+            y: -viewport.height + 0.5,
+            x: isMobile ? 0.3 : 0,
+            z: 7,
+            rotateX: 0,
+            rotateY: isMobile ? -Math.PI / 3 : 0,
+            rotateZ: 0,
+            scaleX: isMobile ? 1.5 : 1,
+            scaleY : isMobile ? 1.5 : 1,
+            scaleZ :isMobile ? 1.5 : 1,
+          },
+          2: {
+            x: isMobile ? -1.4 : -2,
+            y: -viewport.height * 2 + 0.5,
+            z: 0,
+            rotateX: 0,
+            rotateY: Math.PI / 2,
+            rotateZ: 0,
+            scaleX: 1,
+            scaleY : 1,
+            scaleZ : 1,
+          },
+          3: {
+            y: -viewport.height * 3 + 1,
+            x: 0.24,
+            z: 8.5,
+            rotateX: 0,
+            rotateY: -Math.PI / 4,
+            rotateZ: 0,
+            scaleX: 1,
+            scaleY : 1,
+            scaleZ : 1,
+          },
+        }}
+        >
+        <Avatar animation={characterAnimation}/>
+      </motion.group>
+
+      <ambientLight intensity={1}/>
+
+      <motion.group 
+        position={[isMobile ? 0 : 1.5 * responsiveRatio, isMobile ? -viewport.height / 6 : 2, 3]} 
+        scale ={[officeScaleRatio, officeScaleRatio, officeScaleRatio]} 
+        rotation-y={Math.PI/ 4}
+        animate={{
+          y: isMobile ? -viewport.height / 6 : 0,
+        }}
+      >
+        <Office/>
+        <group ref={characterContainerAboutRef}
+         name="CharacterSpot" 
+         position={[1.554, 0.038, -0.093]} 
+         rotation={[0, 1.53, 0]} scale={1.361}>
+        </group>
+      </motion.group>
+
+      <motion.group
+        position={[0, isMobile ? -viewport.height : -1.5 * officeScaleRatio, -10]}
+        animate={{
+          z: section === 1 ? 0 : -10,
+          y: section === 1 ? -viewport.height : (isMobile ? -viewport.height : -1.5*officeScaleRatio),
+        }}
+      >
+        <directionalLight position={[-5, 3, 5]} intensity={0.4} />
+        <Float>
+          <mesh position={[1, -3, -15]} scale={[2, 2, 2]}>
+            <sphereGeometry />
+            <MeshDistortMaterial
+              opacity={0.8}
+              transparent
+              distort={0.4}
+              speed={4}
+              color={"red"}
+            />
+          </mesh>
+        </Float>
+        <Float>
+          <mesh scale={[3, 3, 3]} position={[3, 1, -18]}>
+            <sphereGeometry />
+            <MeshDistortMaterial
+              opacity={0.8}
+              transparent
+              distort={1}
+              speed={5}
+              color="yellow"
+            />
+          </mesh>
+        </Float>
+        <Float>
+          <mesh scale={[1.4, 1.4, 1.4]} position={[-3, -1, -11]}>
+            <boxGeometry />
+            <MeshWobbleMaterial
+              opacity={0.8}
+              transparent
+              factor={1}
+              speed={5}
+              color={"blue"}
+            />
+          </mesh>
+        </Float>
+      </motion.group>
+      <Projects />
+    </>
+  );
+};
